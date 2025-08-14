8dc89e879bf461c7e802d5486144facdddb0cea4
zukoslim <deirmama.best0@gmail.com>
2025-08-14 10:58:24 +0100

    deploys

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,50 +1,30 @@
-import NextAuth from "next-auth";
+
+import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-    providers: [
-  GitHubProvider({
-    clientId: process.env.GITHUB_ID ?? "",
-    clientSecret: process.env.GITHUB_SECRET ?? "",
-  }),
-   CredentialsProvider({
-    // The name to display on the sign in form (e.g. 'Sign in with...')
-    name: 'Credentials',
-    // The credentials is used to generate a suitable form on the sign in page.
-    // You can specify whatever fields you are expecting to be submitted.
-    // e.g. domain, username, password, 2FA token, etc.
-    // You can pass any HTML attribute to the <input> tag through the object.
-    credentials: {
-      email: {},
-      password: {}
-    },
-    async authorize(credentials,) {
-      // You need to provide your own logic here that takes the credentials
-      // submitted and returns either a object representing a user or value
-      // that is false/null if the credentials are invalid.
-      // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-      // You can also use the `req` object to obtain additional parameters
-      // (i.e., the request IP address)
-      const res = await fetch("/your/endpoint", {
-        method: 'POST',
-        body: JSON.stringify(credentials),
-        headers: { "Content-Type": "application/json" }
-      })
-      const user = await res.json()
-
-      // If no error and we have user data, return it
-      if (res.ok && user) {
-        return user
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const res = await fetch("/your/endpoint", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" }
+        });
+        const user = await res.json();
+        if (res.ok && user) return user;
+        return null;
       }
-      // Return null if user data could not be retrieved
-      return null
-    }
-  })
-
-]
-
-}
-
-export const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+    })
+  ]
+};
